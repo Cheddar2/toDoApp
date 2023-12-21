ebca42bb1b986f535db8689debfd6363f04ca6a9
Cheddar2 <cesaraugustoschenattodossantos@gmail.com>
2023-12-21 20:19:28 +0000

    JanKenPo

---
+++ JoKenPo/index.js
@@ -0,0 +1,21 @@
+function computerMove() {
+  const randomValue = Math.random();
+  if (randomValue >= 0 && randomValue < 1 / 3) {
+    return "rock";
+  }
+  if (randomValue >= 1 / 3 && randomValue < 2 / 3) {
+    return "paper";
+  }
+  if (randomValue >= 2 / 3 && randomValue <= 1) {
+    return "scissors";
+  }
+}
+
+function playerMove(event) {
+  if (event.target.closest(".buttons")) {
+  }
+}
+
+function compareMoves() {}
+
+function updateScores() {}
