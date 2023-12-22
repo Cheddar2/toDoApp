cd3f61cd72dc1f851eb8b86126dbc16f1c7d73e8
Cheddar2 <cesaraugustoschenattodossantos@gmail.com>
2023-12-22 19:44:48 +0000

    JKPv1

--- JoKenPo/index.js
+++ JoKenPo/index.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonsContainer = document.querySelector(".buttons");
+  buttonsContainer.addEventListener("click", playerMove);
+});
+
 function computerMove() {
   const randomValue = Math.random();
   if (randomValue >= 0 && randomValue < 1 / 3) {
@@ -13,11 +18,42 @@ function computerMove() {
 
 function playerMove(event) {
   if (event.target.closest(".buttons")) {
+    const userChoice = event.target.classList[0];
+    console.log("User choice:", userChoice);
+    const computerChoice = computerMove();
+    const result = compareMoves(userChoice, computerChoice);
+    updateScores(result);
   }
 }
 
-function compareMoves() {}
+function compareMoves(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "tie";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "win";
+  } else {
+    return "loss";
+  }
+}
 
-function updateScores() {}
+function updateScores(result) {
+  const winsElement = document.getElementById("wins");
+  const lossesElement = document.getElementById("losses");
+  const tiesElement = document.getElementById("ties");
 
-console.log("test1");
+  switch (result) {
+    case "win":
+      winsElement.textContent = parseInt(winsElement.textContent) + 1;
+      break;
+    case "loss":
+      lossesElement.textContent = parseInt(lossesElement.textContent) + 1;
+      break;
+    case "tie":
+      tiesElement.textContent = parseInt(tiesElement.textContent) + 1;
+      break;
+  }
+}
